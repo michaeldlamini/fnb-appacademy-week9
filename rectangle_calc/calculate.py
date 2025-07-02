8f4f9c7634813c7a8296eb26493e4d685ae0bafd
michaeldlamini <michaelmini67@gmail.com>
2025-07-02 09:45:21 +0200

    Create calculate.py

---
+++ rectangle_calc/calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #Functions to calculate rectangle area
+    return length * width
+
+def perimeter(length, width):
+    #Functions to calculate perimeter
+    return 2 *(length + width)
