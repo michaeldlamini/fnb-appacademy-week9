b152a7a64e57d6632ed889d833ebb0fc7973a842
michaeldlamini <michaelmini67@gmail.com>
2025-07-02 09:36:04 +0200

    Create modules_builtin.py

---
+++ modules_builtin.py
@@ -0,0 +1,11 @@
+import random
+import math
+import datetime
+import os
+import sys
+
+#print(random.randint(6, 13))
+#print(math.pi)
+#print(datetime.datetime.now())
+#print(os.getcwd())
+print(sys.version)
